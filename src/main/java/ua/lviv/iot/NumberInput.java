5d793fb199f3f072e166d1f4e3a383ba0e79e0f3
Your Name <andrushkevych2424@gmail.com>
2019-05-07 10:49:14 +0300

    done Lab 7

--- src/main/java/ua/lviv/iot/NumberInput.java
+++ src/main/java/ua/lviv/iot/NumberInput.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 
 public class NumberInput {
 	private String input;
-	private int sum;
-	private List<String> number = new ArrayList<>();
+	private Integer sum;
+	private List<Integer> number = new ArrayList<>();
 	private String numberPattern = "\\d+";
 
 	public NumberInput(String input) {
@@ -21,21 +21,21 @@ public class NumberInput {
 	}
 
 
-	public List<String> findNumber() {
+	public List<Integer> findNumber() {
 		Pattern pattern = Pattern.compile(numberPattern);
 		Matcher matcher = pattern.matcher(input);
 		while (matcher.find()) {
-			sum += matcher.groupCount();
-			number.add(matcher.group());
-			System.out.println(sum);
-
+			number.add(Integer.parseInt(matcher.group()));
 		}
 		return number;
 	}
 
 	public void showResults() {
-		System.out.println("I found: ");
-		System.out.println(this.findNumber());
+		sum = 0;
+		for (Integer num : findNumber()) {
+			sum += num;
+		}
+		System.out.println("Sum is " + sum.toString());
 	}
 
 	public static String readInputNumber() {
